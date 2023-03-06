c911227bc93c7e828f3d47f399ed29c9236f7169
Rene Funcion <rcfun959@gmail.com>
2023-03-05 18:10:44 -0800

    Completed Objects lab.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Rene",
+    streetAddress: "Baseline Road",
+};
+function updateEmployeeWithKeyAndValue(emp, key, value) {
+    const newEmp = { ...emp };
+    newEmp[key] = value;
+    return newEmp;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+    emp[key] = value;
+    return emp;
+}
+function deleteFromEmployeeByKey(emp, key) {
+    const newEmp = { ...emp };
+    delete newEmp[key];
+    return newEmp;
+}
+function destructivelyDeleteFromEmployeeByKey(emp, key) {
+    delete emp[key];
+    return emp;
+}
